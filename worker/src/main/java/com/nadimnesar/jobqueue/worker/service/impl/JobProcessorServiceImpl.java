825914f341fad2d6cb365b99703cee5f036a4214
nadimnesar <nadimnesar.cse@gmail.com>
2025-03-22 22:40:12 +0600

    Reset job progress to 0% when updating status to PROCESSING

--- worker/src/main/java/com/nadimnesar/jobqueue/worker/service/impl/JobProcessorServiceImpl.java
+++ worker/src/main/java/com/nadimnesar/jobqueue/worker/service/impl/JobProcessorServiceImpl.java
@@ -129,8 +129,10 @@ public class JobProcessorServiceImpl implements JobProcessorService {
         jobRepository.save(job);
     }
 
+    //In this implementation, job isn't resuming from the last progress, it's starting from 0% again.
     private void updateJobAsProcessing(JobEntity job) {
         job.setStatus(JobStatus.PROCESSING);
+        job.setCurrentProgress(0);
         job.setStartedAt(LocalDateTime.now());
         job.setCurrentRetryAttemptCount(job.getCurrentRetryAttemptCount() + 1);
         jobRepository.save(job);
