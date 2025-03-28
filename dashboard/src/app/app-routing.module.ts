d43fec3d9392a570b82779d42e695b8d8a3dee4d
nadimnesar <nadimnesar.cse@gmail.com>
2025-03-28 22:55:38 +0600

    Add dashboard component and integrate Bootstrap for styling

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: DashboardComponent,
+        data: {
+            title: 'Dashboard | Distributed Job Queue'
+        }
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
--- dashboard/src/app/app.component.ts
+++ dashboard/src/app/app.component.ts
@@ -1,11 +1,31 @@
 import {Component} from '@angular/core';
+import {Title} from "@angular/platform-browser";
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
+import {filter, map} from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    standalone: false,
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'dashboard';
+    constructor(
+        private titleService: Title,
+        private router: Router,
+        private activatedRoute: ActivatedRoute
+    ) {
+        this.router.events.pipe(
+            filter(event => event instanceof NavigationEnd),
+            map(() => {
+                let route = this.activatedRoute;
+                while (route.firstChild) {
+                    route = route.firstChild;
+                }
+                return route.snapshot.data['title'] || 'Distributed Job Queue';
+            })
+        ).subscribe((title: string) => {
+            this.titleService.setTitle(title);
+        });
+    }
 }
