11b221ffea20e51f7ae573767f0c8fd8b3452470
nadimnesar <nadimnesar.cse@gmail.com>
2025-03-28 23:33:39 +0600

    Implement initial design of dashboard with job statistics, recent jobs, and worker nodes display

--- dashboard/src/app/components/navbar/navbar.component.ts
+++ dashboard/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-navbar',
-  standalone: false,
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+    selector: 'app-navbar',
+    standalone: false,
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+    isLoading = false;
 
+    constructor(private router: Router) {
+    }
+
+    refreshDashboard(): void {
+        this.isLoading = true;
+        const currentUrl = this.router.url;
+        this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+            this.router.navigate([currentUrl]).then(() => {
+                setTimeout(() => {
+                    this.isLoading = false;
+                }, 500);
+            });
+        });
+    }
 }
