43a302b73c34f5149739f8d9a6265ed186c7ba79
nadimnesar <nadimnesar.cse@gmail.com>
2025-04-02 14:48:35 +0600

    Refactor DashboardServiceImpl to use Redisson for Redis operations and update Redis key pattern for worker health

--- producer/src/main/java/com/nadimnesar/jobqueue/producer/dto/CommonResponse.java
+++ producer/src/main/java/com/nadimnesar/jobqueue/producer/dto/CommonResponse.java
@@ -1,6 +1,5 @@
 package com.nadimnesar.jobqueue.producer.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
@@ -15,7 +14,6 @@ public class CommonResponse {
     @Builder.Default
     private int code = 200;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     private Object data;
 
     public static CommonResponse badRequest(String message) {
