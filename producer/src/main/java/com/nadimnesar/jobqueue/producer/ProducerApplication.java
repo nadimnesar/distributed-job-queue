74f3c7b135b33e47d7eb8bbfd2e539dc856e8a79
nadimnesar <nadimnesar.cse@gmail.com>
2025-04-05 18:24:18 +0600

    Refactor logging configuration to use instance-id instead of hostname for producer and worker services

--- producer/src/main/java/com/nadimnesar/jobqueue/producer/ProducerApplication.java
+++ producer/src/main/java/com/nadimnesar/jobqueue/producer/ProducerApplication.java
@@ -3,14 +3,13 @@ package com.nadimnesar.jobqueue.producer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.util.UUID;
 
 @SpringBootApplication(scanBasePackages = "com.nadimnesar.jobqueue")
 public class ProducerApplication {
-    public static void main(String[] args) throws UnknownHostException {
-        // Set the hostname system property to the local host name
-        System.setProperty("hostname", InetAddress.getLocalHost().getHostName());
+    public static void main(String[] args) {
+        // Set a unique instance id for the producer
+        System.setProperty("instance-id", UUID.randomUUID().toString().substring(0, 12));
 
         // Start the Spring Boot application
         SpringApplication.run(ProducerApplication.class, args);
