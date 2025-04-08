dd17abe7e877836c41fb2b3da25c96d74b3d6118
nadimnesar <nadimnesar.cse@gmail.com>
2025-04-09 00:10:11 +0600

    Add application configuration and hostname setup for producer and worker test

--- producer/src/test/java/com/nadimnesar/jobqueue/producer/ProducerApplicationTests.java
+++ producer/src/test/java/com/nadimnesar/jobqueue/producer/ProducerApplicationTests.java
@@ -1,13 +1,21 @@
 package com.nadimnesar.jobqueue.producer;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootTest
 class ProducerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeAll
+    static void setUp() throws UnknownHostException {
+        System.setProperty("hostname", InetAddress.getLocalHost().getHostName());
+    }
 
+    @Test
+    void contextLoads() {
+    }
 }
--- worker/src/test/java/com/nadimnesar/jobqueue/worker/WorkerApplicationTests.java
+++ worker/src/test/java/com/nadimnesar/jobqueue/worker/WorkerApplicationTests.java
@@ -1,13 +1,21 @@
 package com.nadimnesar.jobqueue.worker;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootTest
 class WorkerApplicationTests {
 
+    @BeforeAll
+    static void setUp() throws UnknownHostException {
+        System.setProperty("hostname", InetAddress.getLocalHost().getHostName());
+    }
+
     @Test
     void contextLoads() {
     }
-
 }
