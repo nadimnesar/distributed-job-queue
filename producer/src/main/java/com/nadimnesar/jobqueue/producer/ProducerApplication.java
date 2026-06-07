8e1187fcae5326e9cd53fae77f0acd2fe6a5cd40
nadimnesar <nadimnesar.cse@gmail.com>
2026-06-07 23:03:27 +0600

    Update logging patterns

--- producer/src/main/java/com/nadimnesar/jobqueue/producer/ProducerApplication.java
+++ producer/src/main/java/com/nadimnesar/jobqueue/producer/ProducerApplication.java
@@ -3,15 +3,9 @@ package com.nadimnesar.jobqueue.producer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 @SpringBootApplication(scanBasePackages = "com.nadimnesar.jobqueue")
 public class ProducerApplication {
-    public static void main(String[] args) throws UnknownHostException {
-        // Set hostname as a system property to be used in the application
-        System.setProperty("hostname", InetAddress.getLocalHost().getHostName());
-
+    static void main(String[] args) {
         SpringApplication.run(ProducerApplication.class, args);
     }
 }
--- worker/src/main/java/com/nadimnesar/jobqueue/worker/WorkerApplication.java
+++ worker/src/main/java/com/nadimnesar/jobqueue/worker/WorkerApplication.java
@@ -4,16 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 @SpringBootApplication(scanBasePackages = "com.nadimnesar.jobqueue")
 @EnableScheduling
 public class WorkerApplication {
-    public static void main(String[] args) throws UnknownHostException {
-        // Set hostname as a system property to be used in the application
-        System.setProperty("hostname", InetAddress.getLocalHost().getHostName());
-
+    static void main(String[] args) {
         SpringApplication.run(WorkerApplication.class, args);
     }
 }
